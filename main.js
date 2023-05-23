8144780e6c010da8135cf5f81ab2e5666537e309
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-22 21:14:41 -0700

    feat(main): add rotational psuedo-orbit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Two from 'two.js';
 import { Vector } from 'two.js/src/vector';
+import { Group } from 'two.js/src/group';
 import Body from './modules/Body';
 
 function main() {
@@ -8,20 +9,35 @@ function main() {
     autostart: true,
   };
   const two = new Two(options);
+
+  // Add two to the DOM
   two.appendTo(document.body);
+
+  // instantiate central body
+  const centralBody = new Body(1, 100, 'yellow');
+
+  // instantiate orbiting body
+  const orbitingBody = new Body(0, 10, '#0BDA51');
+  orbitingBody.circle.position.add({ x: 200, y: 0 });
+
+  // Solar System
+  const solarSystem = new Group();
+
+  // Add bodies to the scene-graph
+  centralBody.add(orbitingBody);
+  solarSystem.add(centralBody);
+  two.add(solarSystem);
 
   // origin of coordinate system (0, 0)
   const origin = new Vector(two.width * 0.5, two.height * 0.5);
+  solarSystem.position.add(origin);
 
-  // instantiate central body
-  const centralBody = new Body(1, 100, origin);
+  const angularMomentum = -Math.PI / 360;
+  function update() {
+    orbitingBody.rotation += angularMomentum;
+  }
 
-  // instantiate orbiting body and position to the right of the central body
-  const orbitingBody = new Body(0, 10, Vector.add(origin, Vector.right.multiply(200)));
-
-  // Add bodies to the scene
-  two.add(centralBody);
-  two.add(orbitingBody);
+  two.bind('update', update);
 }
 
 window.addEventListener('load', main);
